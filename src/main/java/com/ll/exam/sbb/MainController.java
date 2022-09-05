ebcffec407260b776ceab59bff6510be08e96f44
brightGarden02 <rreone2017@gmail.com>
2022-09-05 14:51:58 +0900

    feat: 생성자가 여러개면 인자 없는 생성자 선택

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,6 +2,7 @@ package com.ll.exam.sbb;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -250,8 +251,15 @@ class Article {
 
 @AllArgsConstructor
 @Getter
+@Setter
+@NoArgsConstructor
 class Person {
     private int id;
     private int age;
     private String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
 }
