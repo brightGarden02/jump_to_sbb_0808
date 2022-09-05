c64a640cd3933b017e0c9b86550ff65b9c9a737d
brightGarden02 <rreone2017@gmail.com>
2022-09-05 17:31:53 +0900

    feat: 전체조회

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -38,6 +40,19 @@ class SbbApplicationTests {
 
 		assertThat(q1.getId()).isGreaterThan(0);
 		assertThat(q2.getId()).isGreaterThan(q1.getId());
+
+	}
+
+
+	@Test
+	void testJpa2() {
+
+		// select * from question
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 
 	}
 
