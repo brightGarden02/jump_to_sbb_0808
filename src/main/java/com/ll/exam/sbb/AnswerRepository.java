8bb0175018dba6ef8ae58d2efc6b0f16d13eb04c
brightGarden02 <rreone2017@gmail.com>
2022-09-05 17:16:14 +0900

    feat: 리포지토리 추가후 테스트케이스

---
+++ src/main/java/com/ll/exam/sbb/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.exam.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -0,0 +1,6 @@
+package com.ll.exam.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+}
