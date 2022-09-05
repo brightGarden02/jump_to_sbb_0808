b09aa0e32ae5a05aa0ac03743a8f69f0c56807dc
brightGarden02 <rreone2017@gmail.com>
2022-09-05 10:31:31 +0900

    feat: ResponseBody

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,12 +2,17 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/sbb")
-    public void index() {
-        System.out.println("123456");
+
+    @ResponseBody
+    public String index() {
+        System.out.println("Hello");
+        return "안녕하세요~";
     }
+
 }
