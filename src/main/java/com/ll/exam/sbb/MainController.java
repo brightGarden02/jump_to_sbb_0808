e15f746b3324b80b669bb08533b17b4369f4500c
brightGarden02 <rreone2017@gmail.com>
2022-09-05 14:17:35 +0900

    feat: getArticle() 구현

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -139,26 +141,39 @@ public class MainController {
     }
 
 
+    private List<Article> articles = new ArrayList<>();
+
     @GetMapping("/addArticle")
     @ResponseBody
     public String addArticle(String title, String body) {
-//        int id = 1;
-//        Article article = new Article(id, title, body);
 
         Article article = new Article(title, body);
+        articles.add(article);
 
         return "%d번 게시물이 생성되었습니다.".formatted(article.getId());
     }
 
+
+    @GetMapping("/article/{id}")
+    @ResponseBody
+    public Article getArticle(@PathVariable int id) {
+        Article article = articles
+                .stream()
+                .filter(a -> a.getId() == id) // 1번
+                .findFirst()
+                .get();
+
+        return article;
+    }
 }
 
 
 @AllArgsConstructor
+@Getter
 class Article {
 
     private static int lastId = 0;
 
-    @Getter
     private final int id;
     private final String title;
     private final String body;
