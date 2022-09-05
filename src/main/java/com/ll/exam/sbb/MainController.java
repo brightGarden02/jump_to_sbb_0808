5dfc61cfa44613d335c4e37c92ff0095ecf7566b
brightGarden02 <rreone2017@gmail.com>
2022-09-05 10:48:27 +0900

    feat: GetMapping, PostMapping, RequestParam

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,8 +1,7 @@
 package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -13,6 +12,38 @@ public class MainController {
     public String index() {
         System.out.println("Hello");
         return "안녕하세요~";
+    }
+
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2로 POST 방식으로 이동">
+                    </form>
+                """;
+    }
+
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+               <h1>입력된 나이 : %d</h1>
+               <h1>안녕하세요, POST 방식으로 오렸군요.</h1>
+               """.formatted(age);
+    }
+
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+               <h1>입력된 나이 : %d</h1>
+               <h1>안녕하세요, POST 방식으로 오렸군요.</h1>
+               """.formatted(age);
     }
 
 }
