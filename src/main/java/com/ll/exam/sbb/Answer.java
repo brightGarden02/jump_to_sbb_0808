043b1ded3551f386ea3e846a133d46203b3745cc
brightGarden02 <rreone2017@gmail.com>
2022-09-05 16:41:42 +0900

    feat: Answer 클래스 추가

---
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -0,0 +1,26 @@
+package com.ll.exam.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
