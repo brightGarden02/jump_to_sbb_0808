37cb9a91efa09c019bd764c42f9c7fc4e96e7b86
brightGarden02 <rreone2017@gmail.com>
2022-09-05 14:31:12 +0900

    feat: Optional 관련 오류 수정

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -167,7 +167,7 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id) // 1번
                 .findFirst()
-                .get();
+                .orElse(null);
 
         return article;
     }
@@ -180,10 +180,10 @@ public class MainController {
                 .stream()
                 .filter(a-> a.getId() == id) // 1번
                 .findFirst()
-                .get();
+                .orElse(null);
 
         if(article == null) {
-            return "%번 게시물은 존재하지 않습니다.".formatted(article.getId());
+            return "%번 게시물은 존재하지 않습니다.".formatted(id);
         }
 
         article.setTitle(title);
