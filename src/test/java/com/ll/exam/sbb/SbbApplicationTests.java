db1652621a13ff68b66a68cea4e969fabb883d84
brightGarden02 <rreone2017@gmail.com>
2022-09-05 09:35:43 +0900

    feat: 세팅

---
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
