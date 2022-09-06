78c2f1e10836bcbcfdc7a7a1094ac755c84cd185
brightGarden02 <rreone2017@gmail.com>
2022-09-06 23:14:49 +0900

    feat: answerRepository 저장 테스트케이스 추가

---
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.ll.exam.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AnswerRepositoryTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void 저장() {
+        Question q = questionRepository.findById(2).get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        a.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a);
+    }
+
+}
