ede225642277e0496ffa3892082c9451d9c06ef4
brightGarden02 <rreone2017@gmail.com>
2022-09-07 15:45:10 +0900

    feat: 검색조건이 없을 때, 페이지 이동시 검색어에 null 문자열이 들어가는 버그 수정

--- src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
@@ -37,7 +37,7 @@ public class QuestionController {
     @GetMapping("/list")
 //    @ResponseBody
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
-    public String list(String kw, @RequestParam(defaultValue = "") String sortCode, Model model, @RequestParam(defaultValue = "0") int page) {
+    public String list(@RequestParam(defaultValue = "") String kw, @RequestParam(defaultValue = "") String sortCode, Model model, @RequestParam(defaultValue = "0") int page) {
         Page<Question> paging = questionService.getList(kw, page, sortCode);
 
 
