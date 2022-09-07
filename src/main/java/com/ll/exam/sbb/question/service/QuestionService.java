7dbdee526392797dd7c25e07a1e0731eb7d7ac47
brightGarden02 <rreone2017@gmail.com>
2022-09-07 13:04:50 +0900

    feat: 역순 정렬

--- src/main/java/com/ll/exam/sbb/question/service/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/service/QuestionService.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +24,11 @@ public class QuestionService {
 
     public Page<Question> getList(int page) {
 
-        Pageable pageable = PageRequest.of(page,10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+
+        Pageable pageable = PageRequest.of(page,10, Sort.by(sorts));
+
         return this.questionRepository.findAll(pageable);
     }
 
