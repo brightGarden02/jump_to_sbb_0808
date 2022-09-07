ede95941760888e2a3643372d670a1799de60759
brightGarden02 <rreone2017@gmail.com>
2022-09-07 12:36:34 +0900

    feat: 대량 데이터 생성

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -122,6 +123,22 @@ class QuestionRepositoryTests {
         Question q = qList.get(0);
 
         assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
+
+    @Test
+    void createManySampleData() {
+        boolean run = false;
+
+        if(run == false) return;
+
+        IntStream.rangeClosed(3, 300).forEach(id -> {
+            Question q = new Question();
+            q.setSubject("%d번 질문".formatted(id));
+            q.setContent("%d번 질문의 내용".formatted(id));
+            q.setCreateDate(LocalDateTime.now());
+            questionRepository.save(q);
+        });
+
     }
 
 
