6313e7fd38583d605ed1a2c40aa4ea4a4788ad47
brightGarden02 <rreone2017@gmail.com>
2022-09-07 14:48:53 +0900

    feat: 수정폼 처리

--- src/main/java/com/ll/exam/sbb/question/service/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/service/QuestionService.java
@@ -49,4 +49,11 @@ public class QuestionService {
         questionRepository.save(q);
 
     }
+
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        questionRepository.save(question);
+    }
 }
