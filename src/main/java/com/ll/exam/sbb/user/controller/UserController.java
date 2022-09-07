c51067c8e485a3c598dd3a98bf577acd5e190b37
brightGarden02 <rreone2017@gmail.com>
2022-09-07 13:36:41 +0900

    feat: 회원가입 폼, 처리

---
+++ src/main/java/com/ll/exam/sbb/user/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.ll.exam.sbb.user.controller;
+
+import com.ll.exam.sbb.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if(!userCreateForm.getPassword1())
+
+
+    }
+
+
+
+}
