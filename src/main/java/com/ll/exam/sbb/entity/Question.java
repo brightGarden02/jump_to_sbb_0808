18ec5717e99006a58631e926a5004ad848750db8
brightGarden02 <rreone2017@gmail.com>
2022-09-07 10:24:08 +0900

    feat: 문제 : question으로부터_관련된_질문들_조회 를 올바르게 바꿔서 테스트에 통과하도록 만들어보세요.

--- src/main/java/com/ll/exam/sbb/entity/Question.java
+++ src/main/java/com/ll/exam/sbb/entity/Question.java
@@ -23,7 +23,7 @@ public class Question {
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
 
 }
--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -80,6 +82,8 @@ class AnswerRepositoryTests {
     }
 
     @Test
+    @Transactional
+    @Rollback(false)
     void question으로부터_관련된_질문들_조회() {
 
         //SELECT * FROM question WHERE id = 1;
