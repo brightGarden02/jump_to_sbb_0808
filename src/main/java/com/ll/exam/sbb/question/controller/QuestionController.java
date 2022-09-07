96b89239dc62929a40b9f823d43358a850284f25
brightGarden02 <rreone2017@gmail.com>
2022-09-07 12:02:40 +0900

    feat: 질문 작성 폼

--- src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -51,5 +52,9 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 
 }
