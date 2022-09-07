a774409082fbed63d069115d978d006ef42e3e3e
brightGarden02 <rreone2017@gmail.com>
2022-09-07 13:31:49 +0900

    feat: PasswordEncoder 객체의 생성을 스프링이 관리하도록

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
@@ -26,4 +28,11 @@ public class SecurityConfig {
         return http.build();
     }
 
+
+    //스프링 시스템에 객체를 등록한다.
+    //@Configuration라는 어노테이션을 가진 클래스에서만 사용가능
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/ll/exam/sbb/user/service/UserService.java
+++ src/main/java/com/ll/exam/sbb/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.ll.exam.sbb.user.SiteUser;
 import com.ll.exam.sbb.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    // 스프링이 책임지고 PasswordEncoder 타입의 객체를 만들어야 하는 상황
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
