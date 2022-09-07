f86efe97430342f9e4808cceae6a455072deb987
brightGarden02 <rreone2017@gmail.com>
2022-09-07 10:50:19 +0900

    feat: 타임리프 캐시 끄기

--- src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/controller/QuestionController.java
@@ -1,19 +1,42 @@
 package com.ll.exam.sbb.question.controller;
 
+import com.ll.exam.sbb.question.entity.Question;
+import com.ll.exam.sbb.question.repository.QuestionRepository;
 import com.ll.exam.sbb.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor // 생성자 주입
 public class QuestionController {
+
+    // @Autowired //필드주입
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/question/list")
 //    @ResponseBody
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
-    public String list() {
-        return "question list";
+    public String list(Model model) {
+
+        List<Question> questionList = questionRepository.findAll();
+
+        //미래에 실행된 question_list.html에서
+        //questionList라는 이름으로 questionList 변수를 사용할 수 있다
+        model.addAttribute("questionList", questionList);
+
+        return "question_list";
+    }
+
+    @RequestMapping("/question/list2")
+    @ResponseBody
+    public String list2() {
+        return "하하111777";
     }
 
 }
