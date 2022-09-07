273333ef38b023bc191d1e1e29973d43a3b13ff4
brightGarden02 <rreone2017@gmail.com>
2022-09-07 15:12:15 +0900

    feat: 질문과 답변에 추천 테이블 구현

--- src/main/java/com/ll/exam/sbb/answer/entity/Answer.java
+++ src/main/java/com/ll/exam/sbb/answer/entity/Answer.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -28,5 +29,8 @@ public class Answer {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 
 }
--- src/main/java/com/ll/exam/sbb/question/entity/Question.java
+++ src/main/java/com/ll/exam/sbb/question/entity/Question.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -33,6 +34,9 @@ public class Question {
 
     @ManyToOne
     private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 
     public void addAnswer(Answer answer) {
         answer.setQuestion(this);
